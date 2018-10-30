2c6ce26dac8fd39a92271f2f51f307a384ce5be2
Stephane Maarek <simplesteph@users.noreply.github.com>
2018-10-30 12:10:16 +0100

    add read blog

--- src/main/java/com/github/simplesteph/grpc/blog/client/BlogClient.java
+++ src/main/java/com/github/simplesteph/grpc/blog/client/BlogClient.java
@@ -1,9 +1,6 @@
 package com.github.simplesteph.grpc.blog.client;
 
-import com.proto.blog.Blog;
-import com.proto.blog.BlogServiceGrpc;
-import com.proto.blog.CreateBlogRequest;
-import com.proto.blog.CreateBlogResponse;
+import com.proto.blog.*;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 
@@ -38,6 +35,24 @@ public class BlogClient {
 
         System.out.println("Received create blog response");
         System.out.println(createResponse.toString());
+
+        String blogId = createResponse.getBlog().getId();
+
+        System.out.println("Reading blog....");
+
+        ReadBlogResponse readBlogResponse = blogClient.readBlog(ReadBlogRequest.newBuilder()
+                .setBlogId(blogId)
+                .build());
+
+        System.out.println(readBlogResponse.toString());
+
+        // trigger a not found error
+//        System.out.println("Reading blog with non existing id....");
+//
+//        ReadBlogResponse readBlogResponseNotFound = blogClient.readBlog(ReadBlogRequest.newBuilder()
+//                .setBlogId("5bd83b5cf476074e2bff3495")
+//                .build());
+
 
     }
 }
--- src/main/java/com/github/simplesteph/grpc/blog/server/BlogServiceImpl.java
+++ src/main/java/com/github/simplesteph/grpc/blog/server/BlogServiceImpl.java
@@ -4,18 +4,20 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
-import com.proto.blog.Blog;
-import com.proto.blog.BlogServiceGrpc;
-import com.proto.blog.CreateBlogRequest;
-import com.proto.blog.CreateBlogResponse;
+import com.proto.blog.*;
+import io.grpc.Status;
 import io.grpc.stub.StreamObserver;
 import org.bson.Document;
+import org.bson.types.ObjectId;
+
+import static com.mongodb.client.model.Filters.eq;
 
 public class BlogServiceImpl extends BlogServiceGrpc.BlogServiceImplBase {
 
     private MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
     private MongoDatabase database = mongoClient.getDatabase("mydb");
     private MongoCollection<Document> collection = database.getCollection("blog");
+
 
     @Override
     public void createBlog(CreateBlogRequest request, StreamObserver<CreateBlogResponse> responseObserver) {
@@ -44,6 +46,52 @@ public class BlogServiceImpl extends BlogServiceGrpc.BlogServiceImplBase {
         responseObserver.onNext(response);
 
         responseObserver.onCompleted();
+
+    }
+
+
+    @Override
+    public void readBlog(ReadBlogRequest request, StreamObserver<ReadBlogResponse> responseObserver) {
+        System.out.println("Received Read Blog request");
+
+        String blogId = request.getBlogId();
+
+        System.out.println("Searching for a blog");
+        Document result = null;
+
+        try {
+            result = collection.find(eq("_id", new ObjectId(blogId)))
+                    .first();
+        } catch (Exception e) {
+            responseObserver.onError(
+                    Status.NOT_FOUND
+                            .withDescription("The blog with the corresponding id was not found")
+                            .augmentDescription(e.getLocalizedMessage())
+                            .asRuntimeException()
+            );
+        }
+
+        if (result == null) {
+            System.out.println("Blog not found");
+            // we don't have a match
+            responseObserver.onError(
+                    Status.NOT_FOUND
+                            .withDescription("The blog with the corresponding id was not found")
+                            .asRuntimeException()
+            );
+        } else {
+            System.out.println("Blog found, sending response");
+            Blog blog = Blog.newBuilder()
+                    .setAuthorId(result.getString("author_id"))
+                    .setTitle(result.getString("title"))
+                    .setContent(result.getString("content"))
+                    .setId(blogId)
+                    .build();
+
+            responseObserver.onNext(ReadBlogResponse.newBuilder().setBlog(blog).build());
+
+            responseObserver.onCompleted();
+        }
 
     }
 }
